e4fd330b3159272379cbd8af1e742b3208bad627
MikeShahin <gobias101@gmail.com>
2022-03-03 11:48:09 -0800

    updated trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,15 +1,22 @@
-class TripsController < ApplicationController
+class tripsController < ApplicationController
     def index
-
+        trips = trip.all
+        render json: trips
     end
 
     def create
-
-    end
+        @trip = trip.new(trip_params)
+    
+        if @trip.save
+          render json: @trip, status: :created, location: @trip
+        else
+          render json: @trip.errors, status: :unprocessable_entity
+        end
+      end
 
     private
 
     def trip_params
-
+        params.require(:trip).permit(:user, :route, :date, :local_time, :side, :pictures, :comments)
     end
 end

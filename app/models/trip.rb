fcb78630a0c26216615ac81d2df283b876e4f597
MikeShahin <gobias101@gmail.com>
2022-03-03 11:52:47 -0800

    added seeds

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,3 @@
 class Trip < ApplicationRecord
-    validates :user, :route, :date, :local_time, :side, :pictures, :comments
+    validates :user, :route, :date, :local_time, :side, :pictures, :comments, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Trip.create(user: "Mike S.", route: "SFO to DEN", date: "02/20/2022", local_time: "3pm", side: "left", pictures: "https://i.imgur.com/qCNuznE.jpg", comments: "Saw a great view of the Golden Gate Bridge shortly after take-off")
+Trip.create(user: "Mike S.", route: "TRV to MLE", date: "01/06/2016", local_time: "12pm", side: "left", pictures: "https://i.imgur.com/eLXJ0gA.jpg", comments: "Can view the entire island of Male when landing from left side!")
